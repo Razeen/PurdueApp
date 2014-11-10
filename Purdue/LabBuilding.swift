5617932de906af2e6dac0cad121441ef20b4b765
George Lo <twgeolo@gmail.com>
2014-11-09 21:09:57 -0500

    Added GoogleMaps

---
+++ Purdue/LabBuilding.swift
@@ -0,0 +1,13 @@
+//
+//  LabBuilding.swift
+//  Purdue
+//
+//  Created by George Lo on 11/9/14.
+//  Copyright (c) 2014 Purdue University. All rights reserved.
+//
+
+import UIKit
+
+class LabBuilding: NSObject {
+   
+}
---
+++ Purdue/LabRoom.swift
@@ -0,0 +1,13 @@
+//
+//  LabRoom.swift
+//  Purdue
+//
+//  Created by George Lo on 11/9/14.
+//  Copyright (c) 2014 Purdue University. All rights reserved.
+//
+
+import UIKit
+
+class LabRoom: NSObject {
+   
+}
